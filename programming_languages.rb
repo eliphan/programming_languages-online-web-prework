a788ceade14425e5c7e7a988e19f86b03ddf2537
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-15 02:15:47 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,12 @@
 def reformat_languages(languages)
   new_hash = {}
    new_hash[:ruby] = {:type => "interpreted", :style => [:oo]}
-
+   new_hash[:javascript] = {:type => "interpreted", :style => [:oo, :functional]}
+   new_hash[:python] = {:type => "interpreted", :style => [:oo]}
+   new_hash[:java] = {:type => "compiled", :style => [:oo]}
+   new_hash[:clojure] = {:type => "compiled", :style => [:functional]}
+   new_hash[:erlang] = 
+   
 
  new_hash
 end
