ad5c620fbe778d5dae2abdcfa5a4780b1cf47bdc
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-15 02:07:40 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,22 +1,2 @@
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do |language|
-    new_hash[language] = {
-      :ruby => 
-    }
-  
-  
-end
-  new_hash
-end
-
-
-
-result_hash = {}
-hash_from_google["results"].each do |result|
-  result_hash[result["id"]] = {
-    :name => result["name"], 
-    :latitude => result["geometry"]["location"]["lat"],
-    :longitude => result["geometry"]["location"]["lng"]
-  }
-end
